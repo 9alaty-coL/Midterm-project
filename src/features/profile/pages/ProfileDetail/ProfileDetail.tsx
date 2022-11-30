14edfb1b419b02d4ec501105445e59d04df18836
DoriTran <tranquocdong308@gmail.com>
2022-11-30 18:51:35 +0700

    Change profile page

---
+++ src/features/profile/pages/ProfileDetail/ProfileDetail.tsx
@@ -0,0 +1,71 @@
+import { FC, memo, useEffect } from 'react';
+import { AppLoadingSpinner } from 'src/components/AppLoadingSpinner';
+import { useAppDispatch, useAppSelector } from 'src/store';
+import { UserActions } from 'src/store/profile/dispatchers';
+import { selectIsUserLoading, selectProfile } from 'src/store/profile/selectors';
+
+import { TextField, FormControl, InputLabel, Select, MenuItem, Button, Paper } from "@mui/material"
+
+import { User } from "src/models/user"
+
+import {useState } from "react"
+
+import style from "./ProfileDetail.module.css";
+
+interface Props {
+    /** Profile . */
+    readonly profile: User;
+}
+
+const ProfileDetailComponent: FC<Props> = ({
+    profile
+}) => {
+    const [newProfile, setNewProfile] = useState({...profile});
+    const [isEditable, setIsEditable] = useState(false)
+
+    const handleSubmit = (event: any) => {
+        event.preventDefault();
+        console.log(newProfile)
+    }
+
+    return (
+        <Paper elevation={5}>
+            <form className={style['profile-container']} onSubmit={handleSubmit}>
+                <div className={style['profile-container-row']}>
+                    <div  className={style['profile-container-title']}>Profile detail: </div>
+                </div>
+
+                <div className={style['profile-container-row']}>
+                    <TextField id="outlined-basic" label="First name" variant="outlined" value={newProfile.firstName} disabled={!isEditable}
+                        onChange={(event: any) => setNewProfile({...newProfile, firstName: event.target.value})}
+                        sx={{width: 250}}/>
+                    <TextField id="outlined-basic" label="Last name" variant="outlined" value={newProfile.lastName}  disabled={!isEditable}
+                        onChange={(event: any) => setNewProfile({...newProfile, lastName: event.target.value})}
+                        sx={{width: 250}}/>         
+                </div>
+                <div className={style['profile-container-row']}>
+                    <TextField id="outlined-basic" label="Year of birth" variant="outlined" value={newProfile.yearOfBirth} type="number"  disabled={!isEditable}
+                        onChange={(event: any) => setNewProfile({...newProfile, yearOfBirth: event.target.value})}
+                        sx={{width: 100}}/>   
+                </div>
+                <div className={style['profile-container-row']}>
+                    <TextField id="outlined-basic" label="Address" variant="outlined" value={newProfile.address} disabled={!isEditable}
+                        onChange={(event: any) => setNewProfile({...newProfile, address: event.target.value})}
+                        sx={{width: "60vw"}}/>   
+                </div>            
+                <div className={style['profile-btn-wrapper-paper']}>
+                    {!isEditable && <Button variant="outlined" onClick={() => setIsEditable(true)}>Edit profile</Button>}
+                    {isEditable &&
+                    <div className={style['profile-btn-wrapper']}>
+                        <Button variant="contained" color="error" onClick={() => setIsEditable(false)}>Cancel</Button>
+                        <Button variant="contained" color="primary" type="submit">Update</Button>
+                    </div>}
+                </div>  
+            </form>        
+  
+        </Paper>
+
+    )
+}
+
+export const ProfileDetail = memo(ProfileDetailComponent);
--- src/features/profile/pages/ProfilePage.tsx
+++ src/features/profile/pages/ProfilePage.tsx
@@ -4,14 +4,19 @@ import { useAppDispatch, useAppSelector } from 'src/store';
 import { UserActions } from 'src/store/profile/dispatchers';
 import { selectIsUserLoading, selectProfile } from 'src/store/profile/selectors';
 
+import { ProfileDetail } from "./ProfileDetail/ProfileDetail"
+
+import { Avatar } from "@mui/material"
+
 import style from "./ProfilePage.module.css";
 
 const ProfilePageComponent: FC = () => {
   const dispatch = useAppDispatch();
-  const me = useAppSelector(selectProfile)
+  const profile = useAppSelector(selectProfile)
   const isLoading = useAppSelector(selectIsUserLoading)
+
   useEffect(() => {
-    if (me == null) {
+    if (profile == null) {
       dispatch(UserActions.fetchProfile())
     }
   })
@@ -19,28 +24,27 @@ const ProfilePageComponent: FC = () => {
     return <AppLoadingSpinner />
   }
 
-  if (me == null) {
+  if (profile == null) {
     return <h1>User not found</h1>
   }
 
-  return <div className={style['profile']}>
-    <img src={me.avatar} />
-    <div className={style['info']}>
-    <span className={style['title']}>First name: </span>{me.firstName}
+  return (
+    <div className={style['profile-container']}>
+      <div className={style['profile-info-wrapper']}>
+        <Avatar alt="User Avatar" src={profile.avatar} sx={{ width: 150, height: 150 }}/>
+        <div className={style['profile-info']}>
+          <span className={style['profile-name']}>{profile.lastName} {profile.firstName}</span>
+          <span className={style['profile-content']}><b>Email:</b> {profile.email}</span>
+        </div>
+        <div className={style['profile-group']}>
+          <span className={style['profile-content']}><b></b>Owner of {profile.owner.length} groups</span>
+          <span className={style['profile-content']}>Co-Owner of {profile.co_owner.length} groups</span>
+          <span className={style['profile-content']}>Has joined {profile.member.length} groups</span>          
+        </div>
+      </div>
+      <ProfileDetail profile={profile} />
     </div>
-    <div className={style['info']}>
-    <span className={style['title']}>Last name: </span>{me.lastName}
-    </div>
-    <div className={style['info']}>
-    <span className={style['title']}>Email: </span>{me.email}
-    </div>
-    <div className={style['info']}>
-    <span className={style['title']}>Nam sinh: </span>{me.yearOfBirth}
-    </div>
-    <div className={style['info']}>
-    <span className={style['title']}>Dia chi: </span>{me.address}
-    </div>
-  </div>
+  )
 }
 
 export const ProfilePage = memo(ProfilePageComponent);
