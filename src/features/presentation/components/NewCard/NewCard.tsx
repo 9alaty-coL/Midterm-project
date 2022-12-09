969d341477dff50e20dc39c8e0b77311f98aa85c
DoriTran <tranquocdong308@gmail.com>
2022-12-09 11:55:43 +0700

    fix newcard component

--- src/features/presentation/components/NewCard/NewCard.tsx
+++ src/features/presentation/components/NewCard/NewCard.tsx
@@ -5,13 +5,15 @@ import { Paper, Divider, IconButton } from "@mui/material"
 import ControlPointIcon from '@mui/icons-material/ControlPoint';
 
 const NewCardComponent: FC<any> = ({
-    totalPresentation
+    totalPresentation,
+    type
 }) => {
 
     //className={style['']}
     return (
         <div className={style['container']}>
-            <span className={style['title']}>You has {totalPresentation} public presentation</span>
+            { type === "public" && <span className={style['title']}>You have {totalPresentation} public presentations</span> }
+            { type === "group" &&  <span className={style['title']}>Your group has {totalPresentation} presentations</span> }
             <IconButton>
                 <ControlPointIcon sx={{fontSize: 40}}/>
             </IconButton>
