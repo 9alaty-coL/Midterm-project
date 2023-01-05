0a3cb8f87d1666aa2eb2cd431238b2199c565490
9alaty-coL <trantanloc@gmail.com>
2023-01-05 22:49:03 +0700

    Add error handling for question

--- src/features/presentation/components/PresentPage/QuestionBox/QuestionCard/QuestionCard.tsx
+++ src/features/presentation/components/PresentPage/QuestionBox/QuestionCard/QuestionCard.tsx
@@ -5,6 +5,10 @@ import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import { QuestionApiService } from 'src/api/services/question-api';
 import { useAppSelector } from 'src/store';
 import { selectProfile } from 'src/store/profile/selectors';
+import { useSnackbar } from 'notistack';
+import { AxiosError } from 'axios';
+
+const CHECK_VOTE_KEY = 'CHECK_VOTED_KEY'
 
 const QuestionCardComponent: FC<any> = ({
     side,
@@ -17,11 +21,28 @@ const QuestionCardComponent: FC<any> = ({
     setUpdateQuestion,
 }) => {
     const profile = useAppSelector(selectProfile)
+    const { enqueueSnackbar } = useSnackbar();
     const questionBtnClick = () => {
         if (side === 'join') {
+            if (profile == null) {
+                const votedQuestions = JSON.parse(localStorage.getItem(CHECK_VOTE_KEY) ?? '[]')
+                if (votedQuestions?.includes(questionId)) {
+                    enqueueSnackbar('This user already voted this question', {variant: 'error'})
+                    return;
+                }
+                localStorage.setItem(CHECK_VOTE_KEY, JSON.stringify([...votedQuestions, questionId]))
+            }
             QuestionApiService.voteQuestion(questionId, profile?.id ?? null)
                 .then(question => {
                     setUpdateQuestion(question);
+                })
+                .catch(error => {
+                    console.log(error)
+                    if (error instanceof AxiosError) {
+                        enqueueSnackbar(error.response?.data.message, { variant: 'error' })
+                        return;
+                    }
+                    enqueueSnackbar('Problem happen. Try again latter!', { variant: 'error' })
                 })
         } else {
             QuestionApiService.markAsAnswered(questionId)
